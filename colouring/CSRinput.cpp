4d43488802574b6b14834b588d2f36556168f181
Mohammed Shamil <coolshas124@gmail.com>
2016-03-31 13:05:22 +1000

    CSRInput.cpp fixed.

--- colouring/CSRinput.cpp
+++ colouring/CSRinput.cpp
@@ -38,69 +38,95 @@ int main(int argc, char const *argv[])
 		/* code */
 		vertexArray[i]=m;
 	}
-
-	int offset = 0;
-
-	int current = 0;
-	int mark = 1;
-
-	for (int i = 0; i < m; ++i)
-	{
-		/* code */
-		char c;
-		int start;
-		int end;
-
-		cin>>c>>start>>end;
-
-		if (start!=mark){ 
-
-			if (start == mark+1 && vertexArray[mark-1]!=m){ 
-
-			}
-
-			else{
-
-				for (int j = mark; j<start; j++){ 
-					vertexArray[j-1]=offset;
-					// neighbourArray[offset]=0;
-					// offset++;
-				}
-			}
-			mark = start;
-
+	
+	int NSlast = 0;
+	int NSoffset = 0;
+	int NSprev=0;
+	
+	
+	for (int i=0; i<m; i++){
+		int start, end;
+		cin>>start>>end;
+		
+		for (int j=NSlast+1; j<start; j++){
+			vertexArray[j-1]=NSoffset;
+			
 		}
-
-		if (start==current){ 
-			neighbourArray[offset]=end;
-			offset++;
+		
+		if (NSprev!=start){
+			NSlast=start;
+			vertexArray[start-1]=NSoffset;
+			NSprev=start;
 		}
-
-		else { 
-			current = start;
-
-			vertexArray[current-1]=offset;
-
-			neighbourArray[offset]=end;
-			offset++;
-		}
+		
+		neighbourArray[NSoffset]=end;
+		NSoffset++;
+		
+		
 	}
+
+//	int offset = 0;
+
+//	int current = 0;
+//	int mark = 1;
+
+//	for (int i = 0; i < m; ++i)
+//	{
+//		/* code */
+//		char c;
+//		int start;
+//		int end;
+
+//		cin>>c>>start>>end;
+
+//		if (start!=mark){ 
+
+//			if (start == mark+1 && vertexArray[mark-1]!=m){ 
+
+//			}
+
+//			else{
+
+//				for (int j = mark; j<start; j++){ 
+//					vertexArray[j-1]=offset;
+//					// neighbourArray[offset]=0;
+//					// offset++;
+//				}
+//			}
+//			mark = start;
+
+//		}
+
+//		if (start==current){ 
+//			neighbourArray[offset]=end;
+//			offset++;
+//		}
+
+//		else { 
+//			current = start;
+
+//			vertexArray[current-1]=offset;
+
+//			neighbourArray[offset]=end;
+//			offset++;
+//		}
+//	}
 
 	for (int i = 0; i < n; ++i)
 	{
-//		cout<<vertexArray[i]<<" ";
+		cout<<vertexArray[i]<<" ";
 		/* code */
 	}
 
-//	cout<<endl;
+	cout<<endl;
 
 	for (int i = 0; i < m; ++i)
 	{
-//		cout<<neighbourArray[i]<<" ";
+		cout<<neighbourArray[i]<<" ";
 		/* code */
 	}
 
-//	cout<<endl;
+	cout<<endl;
 
 	edgesPrint(vertexArray, neighbourArray, n, m);
 
